c7cb603c5917a42f6382d7b37c9b690478bda74c
Lambda_School_Loaner_145 <lambda_school_loaner_145@Lambda-School-Loaner-145s-MacBook-Pro.local>
2019-08-06 12:31:03 -0700

    adding color-changing footer

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-// Your code goes here
+/// Color transition on mouseover in footer:
+
+let colorChg = document.querySelector("footer");
+
+function generateRGB(min, max) {
+    var min		= min || 0;
+    var max		= min || 255;
+    var color	= [];
+    for (var i = 0; i < 3; i++) {
+        var num = random(min, max);
+        color.push(num);
+    }
+    return color;
+}
+
+
+colorChg.addEventListener("mousemove", ((e) => {
+    colorChg.style.background = `${rgb(event)}`;
+}))
+('mousemove', e => {
+
+}
