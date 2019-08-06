2716f49f3dd2d8df5b005cc9c10ee8ab1a8ffc1d
Lambda_School_Loaner_145 <lambda_school_loaner_145@Lambda-School-Loaner-145s-MacBook-Pro.local>
2019-08-06 14:21:01 -0700

    dblClick function added to move words

--- js/index.js
+++ js/index.js
@@ -1,22 +1,34 @@
-/// Color transition on mouseover in footer:
+/// COLOR TRANSITION in footer **mousemove
+/// // Generate the color
+let colorChg = document.querySelector(".footer");
+function rgb() {
+  function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+  return `rgb(${getRndInteger(0, 255)},${getRndInteger(
+    0,
+    255
+  )}, ${getRndInteger(0, 255)})`;
+}
+/// // Set time between change
+setInterval(function() {
+  colorChg.addEventListener(
+    "mousemove",
+    function(event) {
+      colorChg.style.background = `${rgb(event)}`;
+    },
+    500
+  );
+});
 
-let colorChg = document.querySelector("footer");
-
-function generateRGB(min, max) {
-    var min		= min || 0;
-    var max		= min || 255;
-    var color	= [];
-    for (var i = 0; i < 3; i++) {
-        var num = random(min, max);
-        color.push(num);
-    }
-    return color;
+// 
+//  MOVE THE 'Welcome to Fun Bus!' **dblClick
+let funBus = document.querySelector(".intro h2");
+funBus.addEventListener("dblclick", incMargin);
+let amtMargin = 0;
+function incMargin() {
+  amtMargin += 45;
+  funBus.style.marginLeft = `${amtMargin}px`;
+  funBus.style.color = "#AAAA00";
 }
 
-
-colorChg.addEventListener("mousemove", ((e) => {
-    colorChg.style.background = `${rgb(event)}`;
-}))
-('mousemove', e => {
-
-}
